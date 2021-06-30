ad13b38fd68a9366f9b557be1f7edd7c2ac38263
Kazım Nihat Tekiner <nihattekiner@gmail.com>
2021-06-30 13:21:45 +0300

    update

--- src/test/java/com/tp/day01/GetRequest01.java
+++ src/test/java/com/tp/day01/GetRequest01.java
@@ -1,5 +1,6 @@
 package com.tp.day01;
 
+import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import org.junit.Test;
 import static io.restassured.RestAssured.given;
@@ -25,13 +26,35 @@ public class GetRequest01 {
 
         // 2) Expected result olustur.
         // Burda bizden body'de gelen reponse assert etmemiz beklenmemistir. Bu sebeple bu adima gerek yoktur.
+        // Soruda istenilenler body icerisinde olmayan seyler o yuzden expected result beklentisi yoktur.
 
         // 3) Request gonder
         Response response = given().accept("application/json").when().get(url);
+        response.prettyPrint();
+        // json formatinda sectim accept icinde.
+        /*
+            Given : Istenen endpoint sorgusundan once yapilacak gereklilikleri ifade eder.
+            When : Kullanicinin aksiyonunu belirtir.
+            Then : Ciktilari ifade eder.(Assert islemleri genelde the ile yapilir)
+            And : Coklu islemler yapilacaksa kullanilir.
+         */
+        // new objesi neden olusturmadik ? Cunku Response bir interface'dir.
+        // Interface'lerden obje uretilmez.
 
         // 4) Respose al(actual result)
+        // body testi yapmadigim icin actual result'a gerek yoktur.
 
         // 5) Assertion islemini yapalim.
+        response.then().assertThat().
+                statusCode(200).
+                contentType(ContentType.JSON).
+                statusLine("HTTP/1.1 200 OK");
+
+        System.out.println("status code : " + response.getStatusCode());
+        System.out.println(response.getStatusLine());
+        System.out.println(response.getHeaders());
+        System.out.println(response.getContentType());
+
 
     }
 }
